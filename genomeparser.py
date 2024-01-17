acd85e3d07ab43510f7d294c42ba3fc9303ef89f
Sergio Estevao <smestevao11@gmail.com>
2024-01-17 10:44:58 +0100

    feat: generating genomes

--- genomeparser.py
+++ genomeparser.py
@@ -1,47 +1,84 @@
 from Bio import SeqIO
-import itertools
 import numpy as np
+import random
+from tqdm import tqdm
 
+num_steps = 100  # You can adjust this value
+k = 15
+nucleotides = ['A', 'C', 'G', 'T']
 
-records = []
 states = {}
-k = 2
-nucleotics = ['A', 'C', 'G', 'T']
-probabilities = np.zeros((len(states), len(states)))
-filename = "data/ecoli_PB_small.fastq"
-
-
-# Returns the conditional probability of a transition from a state to another
-def getProb(fromId, toId):
-    return probabilities[fromId][toId] / sum(probabilities[fromId])
-
-
-# Initialize possible states
-counter = 0
-for nuc in itertools.product(nucleotics, repeat=k):
-    states[''.join(nuc)] = counter
-    counter += 1
-
 
 # Parse Genome
 carry = ""
 kmer = ""
+filename = "data/ecoli_PB_small.fastq"
 with open(filename) as handle:
-    for record in SeqIO.parse(handle, "fastq"):
-        seq = carry + record.seq # append the end of the last line to the new record
-        for i in range(len(seq) - k + 1):
+    for record in tqdm(SeqIO.parse(handle, "fastq"), desc="Building states"):
+        seq = record.seq
+        for i in tqdm(range(len(seq) - k + 1), desc="Processing kmers", leave=False):
             old_kmer = kmer
-            kmer = seq[i:i+k]
+            kmer = seq[i:i + k]
 
-            if old_kmer == "": # first kmer has no previous state, skip
-                continue         
+            if old_kmer == "":  # first kmer has no previous state, skip
+                continue
 
-            fromId = states[old_kmer] 
-            toId = states[kmer]
+            if old_kmer not in states:
+                states[old_kmer] = len(states)
 
-            probabilities[fromId][toId] += 1
-            
-        carry = seq[(len(seq) - k + 1):len(seq)] # Carries last (k - 1) letters to next line
+            if kmer not in states:
+                states[kmer] = len(states)
 
+
+# Initialize transition probabilities array as a sparse matrix
+num_states = len(states)
+probabilities = np.zeros((num_states, len(nucleotides)))
+
+# Calculate transition probabilities
+with open(filename) as handle:
+    for record in tqdm(SeqIO.parse(handle, "fastq"), desc="Calculating probabilities"):
+        seq = record.seq
+        for i in tqdm(range(len(seq) - k), desc="Processing kmers", leave=False):
+            kmer = seq[i:i + k]
+            next_nucleotide = seq[i + k]
+
+            fromId = states[kmer]
+            nucleotide_index = nucleotides.index(next_nucleotide)
+
+            probabilities[fromId, nucleotide_index] += 1
+
+
+# Normalize the probabilities
+probabilities = probabilities / probabilities.sum(axis=1)[:, np.newaxis]
 
 print(probabilities)
+
+def generate_genome(num_steps):
+    while True:
+        initial_state = random.choice(list(states.keys()))
+        if any(probabilities[states[initial_state], nucleotide_index] > 0
+               for nucleotide_index in range(len(nucleotides))):
+            break
+
+    current_state = initial_state
+    genome = current_state
+
+    for _ in tqdm(range(num_steps - 1), desc="Generating Genome"):
+        nucleotide_index = random.choices(
+            range(len(nucleotides)),
+            weights=[probabilities[states[current_state], nucleotide_index]
+                     for nucleotide_index in range(len(nucleotides))]
+        )[0]
+        next_nucleotide = nucleotides[nucleotide_index]
+
+        genome += next_nucleotide
+        current_state = current_state[1:] + next_nucleotide  # Update the kmer with the new nucleotide
+
+    return genome
+
+# Generate a genome with a given number of steps
+generated_genome = generate_genome(num_steps)
+
+# Print or use the generated genome as needed
+print(generated_genome)
+print(f"genome size: {len(generated_genome)}")
